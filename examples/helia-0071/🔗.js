e5dca85e493b1da47453a26e7c3ac9c532f9e22f
Kristian Nygaard Jensen <it@0071.se>
2023-07-05 15:00:00 +0200

    ERROR:
    file:///home/kristian/projects/helia-examples/node_modules/ipfs-unixfs-importer/dist/src/dag-builder/index.js:30
        throw errCode(new Error('Content was invalid'), 'ERR_INVALID_CONTENT');
                      ^
    
    Error: Content was invalid
        at contentAsAsyncIterable (file:///home/kristian/projects/helia-examples/node_modules/ipfs-unixfs-importer/dist/src/dag-builder/index.js:30:19)
        at file:///home/kristian/projects/helia-examples/node_modules/ipfs-unixfs-importer/dist/src/dag-builder/index.js:50:90
        at AsyncGenerator.next (<anonymous>)
        at Object.bufferImporter (file:///home/kristian/projects/helia-examples/node_modules/ipfs-unixfs-importer/dist/src/dag-builder/buffer-importer.js:9:24)
        at bufferImporter.next (<anonymous>)
        at file:///home/kristian/projects/helia-examples/node_modules/it-batch/dist/src/index.js:15:30
        at AsyncGenerator.next (<anonymous>)
        at parallelBatch (file:///home/kristian/projects/helia-examples/node_modules/it-parallel-batch/dist/src/index.js:8:22)
        at parallelBatch.next (<anonymous>)
        at buildFileBatch (file:///home/kristian/projects/helia-examples/node_modules/ipfs-unixfs-importer/dist/src/dag-builder/file.js:10:22) {
      code: 'ERR_INVALID_CONTENT'
    }
    
    Node.js v20.3.1

--- examples/helia-0071/🔗.js
+++ examples/helia-0071/🔗.js
@@ -1,5 +1,7 @@
 import {store} from './store.js'
 import {p2p} from './peer.js'
+import {get} from './raw.js'
+import { unixfs } from '@helia/unixfs';
 import { createHelia } from 'helia'
 import { dagCbor } from '@helia/dag-cbor'
 import { dagJson } from '@helia/dag-json'
@@ -9,17 +11,25 @@ const helia = await createHelia({
   libp2p: p2p(store.datastore)
 })
 
+const hfs = unixfs(helia)
+const png = get(hfs, 'QmexeAEQKfZTsrJN5pQXqeA5gS2nUGraeJCZWKUhjh6tN4')
+
+const fcid = await hfs.addFile({
+  path: '~/Pictures/tab.png',
+  content: png});
 
 const cid = CID.parse('QmexeAEQKfZTsrJN5pQXqeA5gS2nUGraeJCZWKUhjh6tN4')
 
 const object1 = {
   hello: 'world',
-  link: cid}
+  link: fcid}
 const d = dagCbor(helia)
 const dj = dagJson(helia)
+console.log(d.get(fcid))
 const myImmutableAddress1 = await d.add(object1)
 const myImmutableAddress2 = await dj.add(object1)
 console.log(myImmutableAddress1)
 console.log(myImmutableAddress2)
-const data = await d.get(cid)
+const ro = d.get(myImmutableAddress1)
+const data = await d.get(ro.link)
 console.log(typeof data)
